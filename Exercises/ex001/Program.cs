12b0ca232f1d05dd88f8fa5020a117dc992fcc20
Gabriel Vergílio <125078273+GabrielVSP@users.noreply.github.com>
2024-01-08 17:58:15 -0300

    Basic exercises

---
+++ Exercises/ex001/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ex001
+{
+    
+
+    class Program
+    {
+        static void Main()
+        {
+
+            Console.WriteLine("Olá, mundo!");
+            Console.Write("Estou aprendendo C#");
+
+            Console.WriteLine("Hello, \n world!");
+
+            /*COMMENT*/
+            //COMMENT
+
+            Console.Beep();
+
+        }
+
+    }
+
+} 
---
+++ Exercises/ex002/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ex002
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            int num; //Declaration
+            num = 2; //Initialization
+
+            int num1 = 13;
+
+            string name = "drive";
+            int age = 16;
+            double height = 1.75;
+            bool isMarried = false;
+            char symbol = '+';
+
+            String username = symbol + name;
+
+            Console.WriteLine(num + num1);
+
+            Console.WriteLine($"Your name is {name}");
+            Console.WriteLine($"Your age is {age}");
+            Console.WriteLine($"Your height is {height}m");
+            Console.WriteLine("Married: " + isMarried);
+            Console.WriteLine("Your symbol is: " + symbol);
+
+            Console.WriteLine($"Username: {username}");
+
+            //Console.ReadKey()
+
+        }
+    }
+}
