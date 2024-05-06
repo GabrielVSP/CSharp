ea2805a784594172c31f5b455e6ad77b4250ab6d
Gabriel Vergílio <125078273+GabrielVSP@users.noreply.github.com>
2024-05-06 12:35:08 -0300

    Added: classes exercise

---
+++ Exercises/ex0087/ex0087/Program.cs
@@ -0,0 +1,7 @@
+﻿
+using ex0087;
+
+Student drive = new Student(134, "drive");
+drive.displayDetails();
+drive.patch();
+drive.displayDetails();
---
+++ Exercises/ex0087/ex0087/student.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex0087
+{
+    internal class Student
+    {
+
+        private int id;
+        private string name;
+
+        public Student(int id, string name)
+        {
+            this.id = id;
+            this.name = name;
+        }
+
+        public void displayDetails()
+        {
+            Console.WriteLine($"Student ID: {id} \nStudent Name: {name}");
+        }
+
+        public void patch()
+        {
+            Console.Write("Enter new student ID:");
+            var newId = Console.ReadLine();
+
+            while (newId.GetType() != typeof(int) )
+            {
+
+                try
+                {
+                    
+                    id = int.Parse(newId);
+                    break;
+
+                } catch ( Exception e ) { 
+
+                    Console.WriteLine(e.Message);
+                    Console.Write("Enter new student ID:");
+                    newId = Console.ReadLine();
+
+                }
+
+            }
+
+        }
+
+    }
+
+}
