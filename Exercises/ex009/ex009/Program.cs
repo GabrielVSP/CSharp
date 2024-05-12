ee16f5497c545686f2b8ecd37f39c5f5da7e01fc
Gabriel Vergílio <125078273+GabrielVSP@users.noreply.github.com>
2024-05-12 00:07:52 -0300

    Added: inheritance exercise

---
+++ Exercises/ex009/ex009/Program.cs
@@ -0,0 +1,4 @@
+﻿using ex009;
+
+PayPal payPal = new PayPal();
+payPal.DisplayNum(12);
---
+++ Exercises/ex009/ex009/payment.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex009
+{
+
+    interface PaymentMethodsProps
+    {
+        protected abstract void Pix();
+    }
+
+    internal abstract class paymentMethods
+    {
+        protected abstract void Pix(int num);
+
+    }
+
+    class PayPal : paymentMethods
+    {
+        protected override void Pix(int num)
+        {
+            Console.WriteLine("Hello, world: " + num);
+        }
+
+        public void DisplayNum(int num)
+        {
+            Pix(num);
+        }
+    }
+}
