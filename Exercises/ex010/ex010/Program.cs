8a0d7621269674961cc4a99326069e98bde7e3c2
Gabriel Vergílio <125078273+GabrielVSP@users.noreply.github.com>
2024-12-25 20:11:20 -0300

    Add: exercise about for loops

---
+++ Exercises/ex010/ex010/Program.cs
@@ -0,0 +1,26 @@
+﻿class ForLoop
+{
+
+      static void  Main()
+    {
+
+        Console.WriteLine("Quantos números você irá digitar?");
+        
+        int n = int.Parse(Console.ReadLine());
+        float sum = 0;
+        
+        for(int i = 1; i <= n; i++)
+        {
+
+            Console.WriteLine($"Valor #{i}:");
+            float num = float.Parse(Console.ReadLine());
+
+            sum += num;
+
+        }
+
+        Console.WriteLine($"A soma dos valores é: {sum}");
+
+    }
+
+}
