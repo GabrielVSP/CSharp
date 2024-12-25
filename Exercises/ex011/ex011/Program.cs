107cee11b8d75b1dc9b23e955c20cb2c0799c317
Gabriel Vergílio <125078273+GabrielVSP@users.noreply.github.com>
2024-12-25 20:54:36 -0300

    Add: 3 for loops exercises

---
+++ Exercises/ex011/ex011/Program.cs
@@ -0,0 +1,21 @@
+﻿class OddOrEven
+{
+
+    static void Main()
+    {
+
+        Console.WriteLine("Escolha um número entre 1 e 1000");
+        int num = int.Parse(Console.ReadLine());
+
+        if (num > 1000 || num < 1) throw new Exception("O valor não atende aos requisistos");
+
+        for(int i = 1; i <= num; i++)
+        {
+
+            if (i % 2 != 0) Console.WriteLine(i);
+
+        }
+
+    }
+
+}
---
+++ Exercises/ex012/ex012/Program.cs
@@ -0,0 +1,32 @@
+﻿class InOut
+{
+
+    static void Main()
+    {
+
+        Console.WriteLine("Quantos números você irá digitar?");
+        int n = int.Parse(Console.ReadLine());
+
+        int[] interval = [10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+
+        for (int i = 0; i < n; i++)
+        {
+
+            Console.WriteLine($"Valor #{i}");
+            int num = int.Parse(Console.ReadLine());
+
+            if (interval.Contains(num))
+            {
+
+                Console.WriteLine($"{num} in");
+
+            } else
+            {
+                Console.WriteLine($"{num} out");
+
+            }
+           
+        }
+
+    }
+}
