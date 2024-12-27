a4024c192adff8995f170d43b240c9cdfe0255bb
Gabriel Vergílio <125078273+GabrielVSP@users.noreply.github.com>
2024-12-27 16:38:05 -0300

    Add: exercise about triangles

---
+++ Exercises/ex013/ex013/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace Math {
+
+    class Triangle
+    {
+
+        static void Main()
+        {
+
+            double xA, xB, xC, yA, yB, yC;
+
+            Console.WriteLine("Entre com as medidas do triângulo X:");
+
+            xA = double.Parse(Console.ReadLine());
+            xB = double.Parse(Console.ReadLine());
+            xC = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Entre com as medidas do triângulo Y:");
+
+            yA = double.Parse(Console.ReadLine());
+            yB = double.Parse(Console.ReadLine());
+            yC = double.Parse(Console.ReadLine());
+
+            double areaX = Area(xA, xB, xC);
+            double areaY = Area(yA, yB, yC);
+
+            Console.WriteLine($"Área de X: {areaX}");
+            Console.WriteLine($"Área de Y: {areaY}");
+
+            Console.WriteLine($"Maior área: " + (areaX > areaY ? "Área de X" : "Área de Y") );
+
+        }
+
+        private static double Area(double a, double b, double c)
+        {
+
+            double p = (a + b + c) / 2;
+            double area = System.Math.Sqrt(
+                p * (p - a) * (p - b) * (p - c)
+                );
+
+            return area;
+
+        }
+
+
+    }
+}
