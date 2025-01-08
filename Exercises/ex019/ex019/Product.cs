f298721311de2f9a1a54c58e5c63f239306fdeae
Gabriel Vergílio <125078273+GabrielVSP@users.noreply.github.com>
2025-01-08 09:39:20 -0300

    Add: exercise about constructors

---
+++ Exercises/ex019/ex019/Product.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex019
+{
+    internal class Product
+    {
+
+        private string name;
+        private double price;
+        private int qnt;
+
+        public Product(string name, double price, int qnt)
+        {
+
+            this.name = name;
+            this.price = price;
+            this.qnt += qnt;
+
+        }
+
+        public string Set(string name, double price, int qnt = 0)
+        {
+            this.name = name;
+            this.price = price;
+            this.qnt += qnt;
+
+            return $"Dados do produto: {this.name}, R${this.price}, {this.qnt} unidades, Total: R${GetTotalValue()}";
+
+        }
+
+        public double GetTotalValue()
+        {
+
+            return price * qnt;
+
+        }
+
+        public string AddQnt(int qnt)
+        {
+
+            if (qnt <= 0) throw new Exception("Valor deve ser maior que zero");
+
+            return this.Set(this.name, this.price, qnt);
+
+        }
+
+        public string RemoveQnt(int qnt)
+        {
+
+            if (qnt <= 0) throw new Exception("Valor deve ser maior que zero");
+
+            return this.Set(this.name, this.price, qnt * -1);
+
+        }
+
+        public override string ToString() {
+
+            return $"Dados do produto: {this.name}, R${this.price}, {this.qnt} unidades, Total: R${GetTotalValue()}";
+
+        }
+
+    }
+}
---
+++ Exercises/ex019/ex019/Program.cs
@@ -0,0 +1,38 @@
+﻿using ex019;
+
+namespace ex019
+{
+
+    class Dashboard
+    {
+
+        static void Main()
+        {
+
+            Console.WriteLine("Entre os dados do produto");
+
+            Console.Write("Nome: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Preço: ");
+            double price = double.Parse(Console.ReadLine());
+
+            Console.Write("Quantidade: ");
+            int qnt = int.Parse(Console.ReadLine());
+
+            Product product = new Product(name, price, qnt);
+
+            Console.WriteLine(product);
+            Console.WriteLine("Quantidade de produtos a ser adicionada no estoque: ");
+
+            Console.WriteLine(product.AddQnt(int.Parse(Console.ReadLine())));
+
+            Console.WriteLine("Quantidade de produtos a ser removido do estoque: ");
+
+            Console.WriteLine(product.RemoveQnt(int.Parse(Console.ReadLine())));
+
+        }
+
+    }
+
+}
