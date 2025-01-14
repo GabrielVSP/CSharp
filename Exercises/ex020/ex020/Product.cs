e9ef93f0f60acceefc356d177acc2a56ef2451cb
Gabriel Vergílio <125078273+GabrielVSP@users.noreply.github.com>
2025-01-14 10:24:44 -0300

    Add: exercise about properties

---
+++ Exercises/ex020/ex020/Product.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace ex020
+{
+    internal class Product
+    {
+
+        private string _name;
+        private double _price;
+        private int _qnt;
+
+        public Product(string name, double price, int qnt)
+        {
+
+            this._name = name;
+            this._price = price;
+            this._qnt += qnt;
+
+        }
+
+        public Product(string name, double price)
+        {
+
+            this._name = name;
+            this._price = price;
+
+        }
+
+        public string Name
+        {
+            get { return _name; }
+            set {
+                if (!string.IsNullOrEmpty(value) && value.Length > 1)
+                {
+                    this._name = value;
+                }
+            }
+        }
+
+        public double Price
+        {
+            get { return _price; }
+            set
+            {
+                if (value > 0) { this._price = value; }
+            }
+        }
+
+        //public void SetName(string name)
+        //{
+
+        //    if(!string.IsNullOrEmpty(name) && name.Length > 1)
+        //    {
+        //        this._name = name;
+        //    }
+
+        //}
+
+        public string Set(string name, double price, int qnt = 0)
+        {
+            this._name = name;
+            this._price = price;
+            this._qnt += qnt;
+
+            return $"Dados do produto: {this._name}, R${this._price}, {this._qnt} unidades, Total: R${GetTotalValue()}";
+
+        }
+
+        public double GetTotalValue()
+        {
+
+            return _price * _qnt;
+
+        }
+
+        public string AddQnt(int qnt)
+        {
+
+            if (qnt <= 0) throw new Exception("Valor deve ser maior que zero");
+
+            return this.Set(this._name, this._price, qnt);
+
+        }
+
+        public string RemoveQnt(int qnt)
+        {
+
+            if (qnt <= 0) throw new Exception("Valor deve ser maior que zero");
+
+            return this.Set(this._name, this._price, qnt * -1);
+
+        }
+
+        public override string ToString()
+        {
+
+            return $"Dados do produto: {this._name}, R${this._price}, {this._qnt} unidades, Total: R${GetTotalValue()}";
+
+        }
+
+    }
+}
---
+++ Exercises/ex020/ex020/Program.cs
@@ -0,0 +1,40 @@
+﻿using ex020;
+
+namespace ex020
+{
+
+    class Dashboard
+    {
+
+        static void Main()
+        {
+
+            Console.WriteLine("Entre os dados do produto");
+
+            Console.Write("Nome: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Preço: ");
+            double price = double.Parse(Console.ReadLine());
+
+            Console.Write("Quantidade: ");
+            int qnt = int.Parse(Console.ReadLine());
+
+            Product product = new Product(name, price, qnt);
+
+            product.Name = "a";
+
+            Console.WriteLine(product);
+            Console.WriteLine("Quantidade de produtos a ser adicionada no estoque: ");
+
+            Console.WriteLine(product.AddQnt(int.Parse(Console.ReadLine())));
+
+            Console.WriteLine("Quantidade de produtos a ser removido do estoque: ");
+
+            Console.WriteLine(product.RemoveQnt(int.Parse(Console.ReadLine())));
+
+        }
+
+    }
+
+}
